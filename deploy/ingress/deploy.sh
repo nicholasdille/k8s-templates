0992ed19b76e3729552b4b86e8fe6959be117120
Nicholas Dille <nicholas.dille@mailbox.org>
2020-03-27 23:58:25 +0100

    Harmonized ingress and monitoring

--- deploy/ingress/deploy.sh
+++ deploy/ingress/deploy.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 set -o errexit
 
-if [[ "$#" -ne 1 ]]; then
-    echo "Usage: $0 <cloudflare-key>"
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: $0 <interface> <cloudflare-key>"
     exit 1
 fi
-CF_API_KEY=$1
+INTERFACE=$1
+CF_API_KEY=$2
 
 if ! docker version 2>&1; then
     echo "Error: Docker not running"
     exit 1
 fi
 
-kind create cluster --name test --config deploy/ingress/kind.yaml
+kind create cluster --config deploy/ingress/kind.yaml
 
 kubectl create namespace kapp
 export KAPP_NAMESPACE=kapp
 
-ip address show dev eth0 | grep " inet " | tr -s ' ' | cut -d' ' -f3 | cut -d'/' -f1 \
-| xargs -I{} kubectl label node test-control-plane dille.io/public-ip={}
-
-./bin/ytt \
-    -f app/traefik/base/ \
-    -f app/traefik/kind-affinity/ \
-    -f app/traefik/external-dns/ \
-    -f deploy/ingress/values.yaml \
-    -v cloudflare.key=${CF_API_KEY} \
-| ./bin/kapp deploy --app traefik --file - --yes
+ip address show dev ${INTERFACE} | grep " inet " | tr -s ' ' | cut -d' ' -f3 | cut -d'/' -f1 \
+| xargs -I{} kubectl label node kind-control-plane dille.io/public-ip={}
 
 ./bin/ytt \
     -f app/cert-manager/base/ \
@@ -46,8 +39,9 @@ ip address show dev eth0 | grep " inet " | tr -s ' ' | cut -d' ' -f3 | cut -d'/'
 | ./bin/kapp deploy --app external-dns --file - --yes
 
 ./bin/ytt \
-    -f app/nginx/ \
-    -f overlay/namespace/ \
-    -v namespace=nginx \
+    -f app/traefik/base/ \
+    -f app/traefik/kind-affinity/ \
+    -f app/traefik/init-dns/ \
     -f deploy/ingress/values.yaml \
-| ./bin/kapp deploy --app nginx --file - --yes
+    -v cloudflare.key=${CF_API_KEY} \
+| ./bin/kapp deploy --app traefik --file - --yes
