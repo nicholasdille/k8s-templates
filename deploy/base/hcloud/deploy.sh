f082615c114ba907537cc199971eb278086a00c8
Nicholas Dille <nicholas.dille@mailbox.org>
2020-03-31 16:13:22 +0200

    Closes #21

--- deploy/base/hcloud/deploy.sh
+++ deploy/base/hcloud/deploy.sh
@@ -80,4 +80,6 @@ sleep 10
 ./bin/ytt \
     -f app/grafana/ \
     -f deploy/base/values.yaml \
+    -v grafana.admin.password=$(openssl rand -hex 16) \
+    -v grafana.database.password=$(openssl rand -hex 16) \
 | ./bin/kapp deploy --app grafana --file - --yes
