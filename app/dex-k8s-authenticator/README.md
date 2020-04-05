4c3a09d5f4679a37169c8bc71567f65a0b1ada10
Nicholas Dille <nicholas.dille@mailbox.org>
2020-04-05 12:48:08 +0200

    Implements #28, implements #27

--- app/dex-k8s-authenticator/README.md
+++
@@ -1,3 +0,0 @@
-# App for dex-k8s-authenticator
-
-XXX https://github.com/mintel/dex-k8s-authenticator
--- deploy/auth/deploy.sh
+++ deploy/auth/deploy.sh
@@ -8,34 +8,12 @@ if test -f .env; then
 fi
 
 ./bin/ytt \
-    -f app/openldap/ \
+    -f app/auth/ \
     -f deploy/auth/values.yaml \
     -v openldap.password.admin=${OPENLDAP_ADMIN_PASSWORD} \
-| ./bin/kapp deploy --app openldap --file - --yes
-
-./bin/ytt \
-    -f app/dex/ \
-    -f app/gangway/values.yaml \
-    -f app/dex-k8s-authenticator/values.yaml \
-    -f app/openldap/values.yaml \
-    -f deploy/auth/values.yaml \
     -v dex.admin.password=${DEX_ADMIN_PASSWORD} \
     -v dex.gitlabcom.id=${GITLAB_APPLICATION_ID} \
     -v dex.gitlabcom.secret=${GITLAB_APPLICATION_SECRET} \
-    -v gangway.client.secret=${GANGWAY_CLIENT_SECRET} \
-    -v openldap.password.admin=${OPENLDAP_ADMIN_PASSWORD} \
-| ./bin/kapp deploy --app dex --file - --yes
-
-./bin/ytt \
-    -f app/dex-k8s-authenticator/ \
-    -f app/dex/values.yaml \
-    -f deploy/auth/values.yaml \
-| ./bin/kapp deploy --app k8s-authenticator --file - --yes
-
-./bin/ytt \
-    -f app/gangway/ \
-    -f app/dex/values.yaml \
-    -f deploy/auth/values.yaml \
     -v gangway.session.secret=${GANGWAY_SESSION_SECRET} \
     -v gangway.client.secret=${GANGWAY_CLIENT_SECRET} \
 | ./bin/kapp deploy --app gangway --file - --yes
