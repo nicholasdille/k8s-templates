05b8c44d477116b4dc4c10196488f2a2d37152d5
Nicholas Dille <nicholas.dille@mailbox.org>
2020-04-13 23:04:41 +0200

    Fixed deployment for #32, fixed persistent storage for #22

--- deploy/grafana/deploy.sh
+++ deploy/grafana/deploy.sh
@@ -1,3 +1,7 @@
+if ${KIND_DEPLOY}; then
+    GRAFANA_STORAGE_ARGS="-f overlay/grafana/kind-storage/"
+    : "${GRAFANA_STORAGE_CLASS:=standard}"
+fi
 if ${INGRESS_HTTPS_ENABLED}; then
     GRAFANA_HTTPS_ARGS="-f overlay/grafana/https/"
 fi
@@ -12,7 +16,9 @@ GRAFANA_DATABASE_PASSWORD=$(openssl rand -hex 16)
     -f app/grafana/ \
     ${GRAFANA_HTTPS_ARGS} \
     ${GRAFANA_DNS_ARGS} \
+    ${GRAFANA_STORAGE_ARGS} \
     -f deploy/values.yaml \
     -v grafana.admin.password=${GRAFANA_ADMIN_PASSWORD} \
     -v grafana.database.password=${GRAFANA_DATABASE_PASSWORD} \
+    -v grafana.volume.storageClass=${GRAFANA_STORAGE_CLASS} \
 | ./bin/kapp deploy --app grafana --file - --yes
