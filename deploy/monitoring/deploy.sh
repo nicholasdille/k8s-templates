04332c2a4ed1bfa62029e312565549a9038865c1
Nicholas Dille <nicholas.dille@mailbox.org>
2020-04-17 15:34:38 +0200

    Prevent unnecessary kube-proxy fix for kind

--- deploy/monitoring/deploy.sh
+++ deploy/monitoring/deploy.sh
@@ -1,4 +1,4 @@
-if ${MONITORING_FIX_KUBEPROXY_ENABLED} && ./bin/kubectl -n kube-system get configmaps kube-proxy | grep --quiet --invert-match "metricsBindAddress: 0.0.0.0:10249"; then
+if ! ${KIND_DEPLOY} && ${MONITORING_FIX_KUBEPROXY_ENABLED} && ./bin/kubectl -n kube-system get configmaps kube-proxy | grep --quiet --invert-match "metricsBindAddress: 0.0.0.0:10249"; then
     ./bin/kubectl -n kube-system get configmaps kube-proxy -o yaml | \
         sed 's/metricsBindAddress: 127.0.0.1:10249/metricsBindAddress: 0.0.0.0:10249/' | \
         sed 's/metricsBindAddress: ""/metricsBindAddress: 0.0.0.0:10249/' | \
