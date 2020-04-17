6cb303226ee7a20ecc840899c62de4b636c909ae
Nicholas Dille <nicholas.dille@mailbox.org>
2020-04-17 12:05:11 +0200

    Fixed deployment of monitoring for cert-manager #32

--- deploy/certificates/monitoring.sh
+++ deploy/certificates/monitoring.sh
@@ -2,7 +2,7 @@ if ${CERTIFICATE_MONITORING_ENABLED}; then
     echo "### Deploying certificate monitoring"
     ./bin/ytt \
         -f overlay/cert-manager/monitoring/ \
-        -f app/cert-manager/values.yaml \
+        -f app/cert-manager/base/values.yaml \
         -f deploy/values.yaml \
     | ./bin/kapp deploy --app cert-manager-monitoring --file - --yes
 fi
